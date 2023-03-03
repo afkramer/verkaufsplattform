b118e5c89848a2ba6c6a8de29b01bf8550be4844
afkramer <adri.kramer@gmail.com>
2023-03-03 15:39:46 +0100

    Add logic to save and create customers/products to and from files

--- src/verkaufsplattform/Main.java
+++ src/verkaufsplattform/Main.java
@@ -2,17 +2,8 @@ package verkaufsplattform;
 
 public class Main {
 	public static void main (String[] args) {
-		//SalesProgram salesProgram = new SalesProgram();
-		//salesProgram.run();
-		Address address = new Address(58, "West 2nd Street", "Corning", "NY", "14830");
-		Customer customer = new Customer(123455, "Adriana", "Kramer", address);
-		String jsonCustString = JsonUtility.toJSON(customer);
-		System.out.println(jsonCustString);
-		Customer customer2 = JsonUtility.customerFromJSON(jsonCustString);
-		
-		System.out.println(customer2.getFirstName());
-		System.out.println(customer2.getLastName());
-		System.out.println(customer2.getAddress().getZipCode());
+		SalesProgram salesProgram = new SalesProgram();
+		salesProgram.run();
 	}
 
 }
---
+++ src/verkaufsplattform/PersistenceUtility.java
@@ -0,0 +1,60 @@
+package verkaufsplattform;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public final class PersistenceUtility {
+	
+	private static final String CUSTOMERS_FILE_PATH = "verkaufsplattform/resources/customers.txt";
+	private static final String PRODUCTS_FILE_PATH = "verkaufsplattform/resources/products.txt";
+	
+	private PersistenceUtility() {}
+		
+	public static void saveCustomersToFile(Customer[] customers) {
+		try(BufferedWriter writer = Files.newBufferedWriter(Paths.get(CUSTOMERS_FILE_PATH));){
+			String jsonString;
+			for(Customer customer : customers) {
+				jsonString = JsonUtility.toJSON(customer);
+				writer.write(jsonString);
+				writer.write("\n");
+			}
+		} catch (IOException e) {
+			System.out.println("Error in saving customers.");
+		}
+		
+	}
+	
+	public static Customer[] createCustomersFromFile() {
+		Customer[] customers = null;
+		try(BufferedReader reader = Files.newBufferedReader(Paths.get(CUSTOMERS_FILE_PATH));){
+			Customer customer;
+			String line;
+			while (true) {
+				line = reader.readLine();
+				if (line != null) {
+					customer = JsonUtility.customerFromJSON(line);
+					customers = Utility.addCustomer(customer, customers);
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("Error in creating customers from file.");
+		}
+		return customers;
+	}
+	
+	public static void saveProductsToFile(Product[] products) {
+		String jsonString;
+		try(BufferedWriter writer = Files.newBufferedWriter(Paths.get(PRODUCTS_FILE_PATH));){
+			for (Product product : products) {
+				jsonString = JsonUtility.toJSON(product);
+				writer.write(jsonString);
+				writer.write("\n");
+			}
+		} catch (IOException e) {
+			System.out.println("Error in saving products.");
+		}
+	}
+}
